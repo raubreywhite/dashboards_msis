92f3d21c897a5c3db66c2fc4865d8956f1466c12
Beatriz Valcarcel <GryMaryson.Groneng@fhi.no>
2017-08-01 13:21:21 +0200

    Creating a tset function

--- R/testFunction.R
+++ R/testFunction.R
@@ -7,3 +7,13 @@ MyNewFunction <- function(argument1){
 
   return(rnorm(3))
 }
+
+#' MyNewFunction
+#' This is a function
+#' @param argument1 This is my parameter
+#' @importFrom stats rnorm
+#' @export MyNewFunction2
+MyNewFunction2 <- function(argument1){
+
+  return(argument1)
+}
---
+++ tests/testthat/test_testFunction.R
@@ -0,0 +1,8 @@
+context("Testing some functions")
+
+
+test_that("My first test", {
+  result <- MyNewFunction2(3)
+  expect_equal(result,3)
+})
+
