9d08e520466d3b1c1fc26b8ebc2342c24484a9a0
Gry Groneng <GryMaryson.Groneng@fhi.no>
2017-08-01 13:14:02 +0200

    Testing

---
+++ R/testFunction.R
@@ -0,0 +1,9 @@
+#' MyNewFunction
+#' This is a function
+#' @param argument1 This is my parameter
+#' @importFrom stats rnorm
+#' @export MyNewFunction
+MyNewFunction <- function(argument1){
+
+  return(rnorm(3))
+}
